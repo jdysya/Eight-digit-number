6680d0e7c940e85829235b46861507dff480a040
xiangyu <xyufj@foxmail.com>
2022-12-06 22:50:37 +0800

    好像不是 A*算法

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <el-button @click="stateChange()">Click Me</el-button>
+    <el-button @click="showSomething()">测试</el-button>
+    <el-button @click="singleStep()">但单步执行</el-button></el-button>
 
     <div style="display: flex; margin-top: 20px; height: 100px;">
       <transition name="el-zoom-in-center">
@@ -74,15 +76,20 @@ export default {
   data: () => ({
     // showArray[0]: true,
     initState: [2, 8, 3, 1, 6, 4, 7, 0, 5],
+    // initState: [1, 6, 3, 2, 0, 7, 4, 5, 8],
     moveState: [],
     endState: [1, 2, 3, 8, 0, 4, 7, 6, 5],
+    // endState: [1, 2, 3, 8, 0, 4, 7, 6, 5],
     showArray: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
     directions: [-1, -3, 1, 3], //空格移动的方向
     aStar: {
       level: 1, //A*算法中,记录当前的层数,即g(x)
       visit: new Set(), //保存已生成过的状态
       currentList: [], // 当前层的状态集合,后续需要选择估价函数最小的进行扩展,若最小值有多个则都需要向下扩展
-      currentMin: [] //当前层的估值函数的最小值
+      currentMin: [], //当前层的估值函数的最小值
+      path: [], //记录路径
+      currentChange: [],// 记录每层所进行的操作,方便后面进行复盘
+      currentQueue: [] //当前层的队列,用于扩展节点
     },
     isSuccess: false
   }),
@@ -91,8 +98,17 @@ export default {
   },
   methods: {
     stateChange() {
-      this.getSolution(this.moveState, 1)
-      console.log(this.aStar);
+      this.getSolution(this.moveState, 1, [])
+    },
+    showSomething() {
+      // this.aStar.currentList[0]=666
+      console.log(this.aStar)
+    },
+    /**
+     * 根据事先推算出的路径,单步运行
+     */
+    singleStep() {
+
     },
     /**
      *  启发式搜索
@@ -101,24 +117,56 @@ export default {
      * @param {*} toDoState 移动的状态数组
      * @param {*} level 层数
      */
-    getSolution(toDoState, level) {
-
+    getSolution(toDoState, level, path) {
       let pos = this.getPrecentPos(toDoState) //获取空格位置
-      //初始状态,将空格按四个方向移动,然后取估价函数最小的继续
-      this.directions.forEach(item => {
-        let posMoving = pos + item
-        if (!this.isLegal(pos, posMoving, level)) return //不合法则跳出本次循环,进入下一轮
-      })
-      //该层扩展完毕,需要找到较小的估值函数
-      console.log(`第${level}扩展完毕,最小值为${this.aStar.currentMin}`);
-      level++
-      // this.aStar.currentList[level-1].forEach(item => {
-      //   if (item.state.valuation == this.aStar.currentMin) {
-      //     // 当前结点估价函数最小,进行扩展
-      //     this.getSolution(item.state)
-      //   }
-      // })
+      let posMoving, legalFlag
+      for (let i = 0; i < 4; i++) {
+        posMoving = pos + this.directions[i]
+        legalFlag = this.isLegal(pos, posMoving, level, toDoState)
+        if (!legalFlag.status) continue //位置不合法则跳出本次循环,进入下一轮
+        if (this.isSuccess) {
+          this.aStar.path = path
+          return true //已经找到路径,程序结束
+        }
+      }
 
+      //该层扩展完毕,需要找到较小的估值函数
+      console.log(`第${level}扩展完毕,最小值为${this.aStar.currentMin[level - 1]}`);
+      console.log(this.aStar.currentList[level - 2]);
+      level++
+      [...this.aStar.currentQueue] = this.aStar.currentList[level - 2] //赋值给当前的队列
+      while (this.aStar.currentQueue.length) {
+        let front = this.aStar.currentQueue.shift() //取出队首
+        if (front.valuation == this.aStar.currentMin[level - 2]) {
+          // 如果当前节点是最小值,则进行扩展
+          pos = this.getPrecentPos(front.state) //获取空格位置
+          for (let i = 0; i < 4; i++) {
+            posMoving = pos + this.directions[i]
+            legalFlag = this.isLegal(pos, posMoving, level, front.state)
+            if (!legalFlag.status) continue //位置不合法则跳出本次循环,进入下一轮
+            if (this.isSuccess) {
+              this.aStar.path = path
+              return true //已经找到路径,程序结束
+            }
+          }
+        }
+        if (this.aStar.currentQueue.length == 0) {
+          // 当上一层结点扩展完毕后,将下一层的状态赋给队列
+          level++ // 开始扩展下一层
+          [...this.aStar.currentQueue] = this.aStar.currentList[level - 2] //深拷贝
+
+        }
+      }
+      // if (this.aStar.currentList[level - 2]) {
+      //   this.aStar.currentList[level - 2].forEach((item, index) => {
+      //     if (item.valuation == this.aStar.currentMin[level - 2]) {
+      //       // 当前结点估价函数最小,进行扩展(可能有多个最小的结点)
+      //       console.log('当前节点估值函数比较小');
+      //       path.push(index)
+      //       this.getSolution(item.state, level, path)
+      //     }
+      //   })
+      // }
 
     },
 
@@ -131,7 +179,8 @@ export default {
       console.log("现在点击的是第" + index + "个卡片");
       // 先获取当前空格所在的位置
       let space_pos = this.getPrecentPos(this.initState)
-      if (Math.abs(index - space_pos) == 1 || Math.abs(index - space_pos) == 3) {
+      if ((Math.abs(index - space_pos) == 1 || Math.abs(index - space_pos) == 3)
+        && Math.abs(this.getRowCol(space_pos).col - this.getRowCol(index).col) < 2) {
         console.log("可以移动");
         this.swapELe(space_pos, index)
       }
@@ -205,12 +254,25 @@ export default {
      * 若合法,便直接进行移动
      * @param {*} before  移动前的位置
      * @param {*} after 移动后的位置
+     *
      */
-    isLegal(before, after, level) {
-      let res = true
-      if (after < 0 || after > 8) return false
-      let [...tmpArray] = this.initState //ES6语法,深拷贝
-      console.log(tmpArray);
+    isLegal(before, after, level, toDoState) {
+      let res = {
+        status: 1,
+        msg: '默认状态'
+      }
+      if (after < 0 || after > 8) return {
+        status: false,
+        msg: '越界'
+      }
+      let [...tmpArray] = toDoState //ES6语法,深拷贝
+      // console.log(tmpArray);
+
+      // 要求移动前后的位置相邻,(注意 3 和 2 并不相邻)
+      if (Math.abs(this.getRowCol(before).col - this.getRowCol(after).col) > 1) return {
+        status: false,
+        msg: '相隔列数大于 1'
+      }
 
       // 直接将对应元素互换位置即可
       let temp = tmpArray[before]
@@ -219,22 +281,44 @@ export default {
 
       if (this.aStar.visit.has(tmpArray.join())) {
         // 当前移动的状态已经存在
-        res = false
+        res = {
+          status: false,
+          msg: '已存在当前状态'
+        }
       }
       else {
         // 当前状态合法,将其添加进数组中进行保存
-        console.log(`before=${before},after=${after}`);
+        console.log(`before=${before},after=${after}`)
         let val = level + this.getDistance(tmpArray) //估价函数,当前层数+与目标的曼哈顿距离
         if (!this.aStar.currentList[level - 1]) this.aStar.currentList[level - 1] = []
         this.aStar.currentList[level - 1].push({
           state: tmpArray, //当前状态,数组类型
           valuation: val
         })
-        if (!this.aStar.currentMin[level-1] || val < this.aStar.currentMin[level - 1]) this.aStar.currentMin[level-1] = val
+        if (!this.aStar.currentMin[level - 1] || val < this.aStar.currentMin[level - 1]) this.aStar.currentMin[level - 1] = val
         this.aStar.visit.add(tmpArray.join()) // 将当前的数组加入状态集合
+        if (tmpArray.join() == this.endState.join()) {
+          this.isSuccess = true
+          this.$alert("成功")
+          return {
+            status: 2,
+            msg: '找到最终状态,程序结束',
+            path: {
+              before,
+              after
+            }
+          }
+        }
       }
 
-      return res
+      return {
+        status: 1,
+        msg: "正常结束",
+        path: {
+          before,
+          after
+        }
+      }
 
     },
 
@@ -250,7 +334,7 @@ export default {
             let tmp = Math.abs(this.getRowCol(ind1).row - this.getRowCol(ind2).row)
               + Math.abs(this.getRowCol(ind1).col - this.getRowCol(ind2).col)
             val += tmp
-            console.log(tmp);
+            // console.log(tmp);
             return
           }
         })
@@ -287,6 +371,30 @@ export default {
   },
   mounted() {
     [...this.moveState] = this.initState // 深拷贝
+    this.aStar.visit.add(this.initState.join()) //初始状态加入到已访问的集合中
+    let ls_test = [{
+      'arr': [1, 2, 3],
+      'val': 2
+    },
+    {
+      'arr': [1, 2, 3],
+      'val': 3
+    },
+    {
+      'arr': [1, 2, 3],
+      'val': 1
+    }
+    ]
+    let newls = ls_test.sort((a, b) => {
+      if (a.val > b.val) {
+        return 1
+      } else if (a.val < b.val) {
+        return -1
+      } else {
+        return 0
+      }
+    })
+    console.log(newls);
     // for (let i = 0; i < 8; i++) this.showArray.push([0, 0, 0, 0])
     // console.log(this.showArray);
     // let tmp_res = this.getDistance([2, 8, 3, 1, 6, 4, 0, 7, 5])
