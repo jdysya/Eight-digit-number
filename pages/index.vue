0485beba75626fd21338a4bd89ce623ea41134b6
xiangyu <xyufj@foxmail.com>
2022-12-07 08:58:47 +0800

    基本完成 A*算法

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,99 @@
 <template>
   <div>
-    <el-button @click="stateChange()">Click Me</el-button>
-    <el-button @click="showSomething()">测试</el-button>
-    <el-button @click="singleStep()">但单步执行</el-button></el-button>
 
-    <div style="display: flex; margin-top: 20px; height: 100px;">
-      <transition name="el-zoom-in-center">
-        <div @click="swapSpace(0)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[0][0], 'roll-in-right': showArray[0][1], 'roll-in-top': showArray[0][2], 'roll-in-bottom': showArray[0][3] }">
-          {{ numShow(0) }}</div>
-      </transition>
+    <div class="box"></div>
+    <el-row :gutter="20" type="flex" justify="center">
+      <el-col :span="6">
+        <el-card>
+          <div style="display: flex; margin-top: 20px; height: 100px;">
+            <transition name="el-zoom-in-center">
+              <div @click="swapSpace(0)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[0][0], 'roll-in-right': showArray[0][1], 'roll-in-top': showArray[0][2], 'roll-in-bottom': showArray[0][3] }">
+                {{ numShow(0) }}
+              </div>
+            </transition>
 
-      <transition name="el-zoom-in-top">
-        <div @click="swapSpace(1)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[1][0], 'roll-in-right': showArray[1][1], 'roll-in-top': showArray[1][2], 'roll-in-bottom': showArray[1][3] }">
-          {{ numShow(1) }}</div>
-      </transition>
+            <transition name="el-zoom-in-top">
+              <div @click="swapSpace(1)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[1][0], 'roll-in-right': showArray[1][1], 'roll-in-top': showArray[1][2], 'roll-in-bottom': showArray[1][3] }">
+                {{ numShow(1) }}
+              </div>
+            </transition>
 
-      <transition name="el-zoom-in-bottom">
-        <div @click="swapSpace(2)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[2][0], 'roll-in-right': showArray[2][1], 'roll-in-top': showArray[2][2], 'roll-in-bottom': showArray[2][3] }">
-          {{ numShow(2) }}</div>
-      </transition>
-    </div>
+            <transition name="el-zoom-in-bottom">
+              <div @click="swapSpace(2)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[2][0], 'roll-in-right': showArray[2][1], 'roll-in-top': showArray[2][2], 'roll-in-bottom': showArray[2][3] }">
+                {{ numShow(2) }}
+              </div>
+            </transition>
+          </div>
 
-    <div style="display: flex; margin-top: 20px; height: 100px;">
-      <transition name="el-zoom-in-center">
-        <div @click="swapSpace(3)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[3][0], 'roll-in-right': showArray[3][1], 'roll-in-top': showArray[3][2], 'roll-in-bottom': showArray[3][3] }">
-          {{ numShow(3) }}</div>
-      </transition>
+          <div style="display: flex; margin-top: 20px; height: 100px;">
+            <transition name="el-zoom-in-center">
+              <div @click="swapSpace(3)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[3][0], 'roll-in-right': showArray[3][1], 'roll-in-top': showArray[3][2], 'roll-in-bottom': showArray[3][3] }">
+                {{ numShow(3) }}
+              </div>
+            </transition>
 
-      <transition name="el-zoom-in-top">
-        <div @click="swapSpace(4)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[4][0], 'roll-in-right': showArray[4][1], 'roll-in-top': showArray[4][2], 'roll-in-bottom': showArray[4][3] }">
-          {{ numShow(4) }}</div>
-      </transition>
+            <transition name="el-zoom-in-top">
+              <div @click="swapSpace(4)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[4][0], 'roll-in-right': showArray[4][1], 'roll-in-top': showArray[4][2], 'roll-in-bottom': showArray[4][3] }">
+                {{ numShow(4) }}
+              </div>
+            </transition>
 
-      <transition name="el-zoom-in-bottom">
-        <div @click="swapSpace(5)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[5][0], 'roll-in-right': showArray[5][1], 'roll-in-top': showArray[5][2], 'roll-in-bottom': showArray[5][3] }">
-          {{ numShow(5) }}</div>
-      </transition>
-    </div>
+            <transition name="el-zoom-in-bottom">
+              <div @click="swapSpace(5)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[5][0], 'roll-in-right': showArray[5][1], 'roll-in-top': showArray[5][2], 'roll-in-bottom': showArray[5][3] }">
+                {{ numShow(5) }}
+              </div>
+            </transition>
+          </div>
 
-    <div style="display: flex; margin-top: 20px; height: 100px;">
-      <transition name="el-zoom-in-center">
-        <div @click="swapSpace(6)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[6][0], 'roll-in-right': showArray[6][1], 'roll-in-top': showArray[6][2], 'roll-in-bottom': showArray[6][3] }">
-          {{
-              numShow(6)
-          }}</div>
-      </transition>
+          <div style="display: flex; margin-top: 20px; height: 100px;">
+            <transition name="el-zoom-in-center">
+              <div @click="swapSpace(6)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[6][0], 'roll-in-right': showArray[6][1], 'roll-in-top': showArray[6][2], 'roll-in-bottom': showArray[6][3] }">
+                {{
+                  numShow(6)
+                }}
+              </div>
+            </transition>
 
-      <transition name="el-zoom-in-top">
-        <div @click="swapSpace(7)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[7][0], 'roll-in-right': showArray[7][1], 'roll-in-top': showArray[7][2], 'roll-in-bottom': showArray[7][3] }">
-          {{
-              numShow(7)
-          }}</div>
-      </transition>
+            <transition name="el-zoom-in-top">
+              <div @click="swapSpace(7)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[7][0], 'roll-in-right': showArray[7][1], 'roll-in-top': showArray[7][2], 'roll-in-bottom': showArray[7][3] }">
+                {{
+                  numShow(7)
+                }}
+              </div>
+            </transition>
 
-      <transition name="el-zoom-in-bottom">
-        <div @click="swapSpace(8)"
-          :class="{ 'transition-box': true, 'roll-in-left': showArray[8][0], 'roll-in-right': showArray[8][1], 'roll-in-top': showArray[8][2], 'roll-in-bottom': showArray[8][3] }">
-          {{ numShow(8) }}</div>
-      </transition>
-    </div>
-    <div>当前状态:{{ isSuccess }}</div>
+            <transition name="el-zoom-in-bottom">
+              <div @click="swapSpace(8)"
+                   :class="{ 'transition-box': true, 'roll-in-left': showArray[8][0], 'roll-in-right': showArray[8][1], 'roll-in-top': showArray[8][2], 'roll-in-bottom': showArray[8][3] }">
+                {{ numShow(8) }}
+              </div>
+            </transition>
+          </div>
+          <div>当前状态:{{ isSuccess }}</div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <div class="box"></div>
+    <el-row type="flex" justify="center">
+      <el-button @click="stateChange()">开始</el-button>
+      <el-button @click="showSomething()">测试</el-button>
+      <el-button @click="singleStep()" :disabled='isFinish'>单步执行</el-button>
+    </el-row>
+
+
   </div>
 </template>
 
 <script>
+// import {PriorityQueue} from '../common/queue.js'
 export default {
   data: () => ({
     // showArray[0]: true,
@@ -88,17 +110,16 @@ export default {
       currentList: [], // 当前层的状态集合,后续需要选择估价函数最小的进行扩展,若最小值有多个则都需要向下扩展
       currentMin: [], //当前层的估值函数的最小值
       path: [], //记录路径
-      currentChange: [],// 记录每层所进行的操作,方便后面进行复盘
       currentQueue: [] //当前层的队列,用于扩展节点
     },
-    isSuccess: false
+    isSuccess: false,
+    isFinish: false,
+    currentIndex: 0 //记录单步执行的步数
   }),
-  computed: {
-
-  },
+  computed: {},
   methods: {
     stateChange() {
-      this.getSolution(this.moveState, 1, [])
+      this.getSolution(this.moveState)
     },
     showSomething() {
       // this.aStar.currentList[0]=666
@@ -108,7 +129,14 @@ export default {
      * 根据事先推算出的路径,单步运行
      */
     singleStep() {
-
+      let path = this.aStar.path
+      let toChange = path[this.currentIndex + 1].change
+      this.swapELe(toChange.before, toChange.after)
+      this.currentIndex++
+      if (this.initState.join() == this.endState.join()) {
+        this.isFinish = true
+        this.$alert("已经达到最终状态！")
+      }
     },
     /**
      *  启发式搜索
@@ -117,57 +145,40 @@ export default {
      * @param {*} toDoState 移动的状态数组
      * @param {*} level 层数
      */
-    getSolution(toDoState, level, path) {
-      let pos = this.getPrecentPos(toDoState) //获取空格位置
-      let posMoving, legalFlag
-      for (let i = 0; i < 4; i++) {
-        posMoving = pos + this.directions[i]
-        legalFlag = this.isLegal(pos, posMoving, level, toDoState)
-        if (!legalFlag.status) continue //位置不合法则跳出本次循环,进入下一轮
-        if (this.isSuccess) {
-          this.aStar.path = path
-          return true //已经找到路径,程序结束
-        }
-      }
+    getSolution(toDoState) {
+      let distance = this.getDistance(toDoState)
+      console.log(this.aStar.currentQueue)
+      this.aStar.currentQueue.push({
+        state: toDoState,
+        valuation: distance,//初始的估价函数
+        father: null,
+        level: 0 //结点所在的层数，根节点为 0 层（0 步）
+      })
 
-      //该层扩展完毕,需要找到较小的估值函数
-      console.log(`第${level}扩展完毕,最小值为${this.aStar.currentMin[level - 1]}`);
-      console.log(this.aStar.currentList[level - 2]);
-      level++
-      [...this.aStar.currentQueue] = this.aStar.currentList[level - 2] //赋值给当前的队列
       while (this.aStar.currentQueue.length) {
         let front = this.aStar.currentQueue.shift() //取出队首
-        if (front.valuation == this.aStar.currentMin[level - 2]) {
-          // 如果当前节点是最小值,则进行扩展
-          pos = this.getPrecentPos(front.state) //获取空格位置
-          for (let i = 0; i < 4; i++) {
-            posMoving = pos + this.directions[i]
-            legalFlag = this.isLegal(pos, posMoving, level, front.state)
-            if (!legalFlag.status) continue //位置不合法则跳出本次循环,进入下一轮
-            if (this.isSuccess) {
-              this.aStar.path = path
-              return true //已经找到路径,程序结束
+        // 将取出的结点进行扩展
+
+        // 如果当前节点是最小值,则进行扩展
+        let pos = this.getPrecentPos(front.state) //获取空格位置
+
+        for (let i = 0; i < 4; i++) {
+          let posMoving = pos + this.directions[i]
+          let legalFlag = this.isLegal(pos, posMoving, front)
+          if (!legalFlag.status) continue //位置不合法则跳出本次循环,进入下一轮
+          if (this.isSuccess) {
+            //已经找到路径,程序结束
+            // 此时以队列第一个元素为准，记录路径
+            let endNode = this.aStar.currentQueue.shift()
+            this.aStar.path.unshift(endNode)
+            while (endNode.father) {
+              this.aStar.path.unshift(endNode.father)
+              endNode = endNode.father
             }
+            return true
           }
         }
-        if (this.aStar.currentQueue.length == 0) {
-          // 当上一层结点扩展完毕后,将下一层的状态赋给队列
-          level++ // 开始扩展下一层
-          [...this.aStar.currentQueue] = this.aStar.currentList[level - 2] //深拷贝
-
-        }
       }
-      // if (this.aStar.currentList[level - 2]) {
-      //   this.aStar.currentList[level - 2].forEach((item, index) => {
-      //     if (item.valuation == this.aStar.currentMin[level - 2]) {
-      //       // 当前结点估价函数最小,进行扩展(可能有多个最小的结点)
-      //       console.log('当前节点估值函数比较小');
-      //       path.push(index)
-      //       this.getSolution(item.state, level, path)
-      //     }
-      //   })
-      // }
-
     },
 
     /**
@@ -183,8 +194,7 @@ export default {
         && Math.abs(this.getRowCol(space_pos).col - this.getRowCol(index).col) < 2) {
         console.log("可以移动");
         this.swapELe(space_pos, index)
-      }
-      else {
+      } else {
         this.$alert("当前操作非法!")
       }
     },
@@ -203,8 +213,7 @@ export default {
           this.showArray[nonSpace].splice(1, 1, false)
           this.showArray[space].splice(0, 1, false)
         }, 600);
-      }
-      else if (nonSpace - space == 1) {
+      } else if (nonSpace - space == 1) {
         // 空格右边
         this.showArray[nonSpace].splice(0, 1, true)
         this.showArray[space].splice(1, 1, true)
@@ -212,8 +221,7 @@ export default {
           this.showArray[nonSpace].splice(0, 1, false)
           this.showArray[space].splice(1, 1, false)
         }, 600);
-      }
-      else if (space - nonSpace == 3) {
+      } else if (space - nonSpace == 3) {
         // 空格上边
         this.showArray[nonSpace].splice(2, 1, true)
         this.showArray[space].splice(3, 1, true)
@@ -221,8 +229,7 @@ export default {
           this.showArray[nonSpace].splice(2, 1, false)
           this.showArray[space].splice(3, 1, false)
         }, 600);
-      }
-      else if (nonSpace - space == 3) {
+      } else if (nonSpace - space == 3) {
         // 空格下边
         this.showArray[nonSpace].splice(3, 1, true)
         this.showArray[space].splice(2, 1, true)
@@ -242,10 +249,11 @@ export default {
     /**
      * 获取传入状态空格的当前位置
      */
+
     getPrecentPos(ls) {
       let res = -1
       ls.forEach((item, index) => {
-        if (item == 0) res = index
+        if (item === 0) res = index
       })
       return res
     },
@@ -254,9 +262,10 @@ export default {
      * 若合法,便直接进行移动
      * @param {*} before  移动前的位置
      * @param {*} after 移动后的位置
-     *
+     * @param level
+     * @param toDoState
      */
-    isLegal(before, after, level, toDoState) {
+    isLegal(before, after, toDoState) {
       let res = {
         status: 1,
         msg: '默认状态'
@@ -265,7 +274,7 @@ export default {
         status: false,
         msg: '越界'
       }
-      let [...tmpArray] = toDoState //ES6语法,深拷贝
+      let [...tmpArray] = toDoState.state //ES6语法,深拷贝
       // console.log(tmpArray);
 
       // 要求移动前后的位置相邻,(注意 3 和 2 并不相邻)
@@ -285,19 +294,27 @@ export default {
           status: false,
           msg: '已存在当前状态'
         }
-      }
-      else {
+      } else {
         // 当前状态合法,将其添加进数组中进行保存
         console.log(`before=${before},after=${after}`)
-        let val = level + this.getDistance(tmpArray) //估价函数,当前层数+与目标的曼哈顿距离
-        if (!this.aStar.currentList[level - 1]) this.aStar.currentList[level - 1] = []
-        this.aStar.currentList[level - 1].push({
+        let currentLevel = toDoState.level + 1
+        let val = currentLevel + this.getDistance(tmpArray) //估价函数,当前层数+与目标的曼哈顿距离
+        this.aStar.currentQueue.push({
           state: tmpArray, //当前状态,数组类型
-          valuation: val
-        })
-        if (!this.aStar.currentMin[level - 1] || val < this.aStar.currentMin[level - 1]) this.aStar.currentMin[level - 1] = val
+          valuation: val,
+          father: toDoState,
+          level: currentLevel,
+          change: {
+            before,
+            after
+          }
+        }) //加入队列
+
+        this.mySort(this.aStar.currentQueue)//排序
+
+        // if (!this.aStar.currentMin[level - 1] || val < this.aStar.currentMin[level - 1]) this.aStar.currentMin[level - 1] = val
         this.aStar.visit.add(tmpArray.join()) // 将当前的数组加入状态集合
-        if (tmpArray.join() == this.endState.join()) {
+        if (tmpArray.join() === this.endState.join()) {
           this.isSuccess = true
           this.$alert("成功")
           return {
@@ -330,7 +347,7 @@ export default {
       let val = 0
       ls.forEach((cur, ind1) => {
         this.endState.forEach((tar, ind2) => {
-          if (cur == tar && cur != 0) {
+          if (cur === tar && cur !== 0) {
             let tmp = Math.abs(this.getRowCol(ind1).row - this.getRowCol(ind2).row)
               + Math.abs(this.getRowCol(ind1).col - this.getRowCol(ind2).col)
             val += tmp
@@ -355,7 +372,6 @@ export default {
     },
 
 
-
     /**
      * 0不显示,显示为空
      * @param {*} index
@@ -363,10 +379,20 @@ export default {
     numShow(index) {
       if (this.initState[index] != 0) {
         return this.initState[index]
-      }
-      else {
+      } else {
         return ' '
       }
+    },
+    mySort(ls) {
+      return ls.sort((a, b) => {
+        if (a.valuation > b.valuation) {
+          return 1
+        } else if (a.valuation < b.valuation) {
+          return -1
+        } else {
+          return 0
+        }
+      })
     }
   },
   mounted() {
@@ -376,16 +402,16 @@ export default {
       'arr': [1, 2, 3],
       'val': 2
     },
-    {
-      'arr': [1, 2, 3],
-      'val': 3
-    },
-    {
-      'arr': [1, 2, 3],
-      'val': 1
-    }
+      {
+        'arr': [1, 2, 3],
+        'val': 3
+      },
+      {
+        'arr': [1, 2, 3],
+        'val': 1
+      }
     ]
-    let newls = ls_test.sort((a, b) => {
+    ls_test = ls_test.sort((a, b) => {
       if (a.val > b.val) {
         return 1
       } else if (a.val < b.val) {
@@ -394,14 +420,6 @@ export default {
         return 0
       }
     })
-    console.log(newls);
-    // for (let i = 0; i < 8; i++) this.showArray.push([0, 0, 0, 0])
-    // console.log(this.showArray);
-    // let tmp_res = this.getDistance([2, 8, 3, 1, 6, 4, 0, 7, 5])
-    // console.log(tmp_res);
-    // for(let i =0; i < 9; i++){
-    //   console.log(this.getRowCol(i));
-    // }
   }
 }
 </script>
@@ -420,7 +438,13 @@ export default {
   margin-right: 20px;
 }
 
+.box-card {
+  width: 480px;
+}
 
+.box {
+  height: 50px;
+}
 
 .jello-horizontal {
   -webkit-animation: jello-horizontal 0.9s both;
